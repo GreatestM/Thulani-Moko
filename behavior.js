08eeeb3a25000df5a7efe5bb57ab3d792b2826ab
GreatestM <86297375+GreatestM@users.noreply.github.com>
2021-08-24 14:58:13 +0200

    initial commit

---
+++ behavior.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+	this.classList.toggle('small'); });
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.className ="";
+});
